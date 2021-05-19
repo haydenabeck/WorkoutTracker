3972243e4c6418e1d6df1095c151533a697f8c34
Hayden Beck <haydenabeck@gmail.com>
2021-05-19 17:01:40 -0500

    api baby api

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"));
 app.use(require('./routes/html'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
